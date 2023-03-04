31f2300e6af9fd352fc6c35b83f36fca2c49dbde
云雾 <123147018+yunwuu@users.noreply.github.com>
2023-03-04 23:14:52 +0800

    Update google.js

--- google.js
+++ google.js
@@ -4,11 +4,12 @@ console.log('Powered by Yunwu');
 
 const key = "GF3DA";
 
-window.onload = function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
     if(document.cookie.replace(/(?:(?:^|.*;\s*)key\s*\=\s*([^;]*).*$)|^.*$/, "$1") != key) {
+        console.log("Check failed");
         event.preventDefault();
         document.write("<h1>403</h1>");
     } else {
         console.log("Check passed");
     }
-}
+})
